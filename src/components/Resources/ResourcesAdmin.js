2d3cfa3be5729d552c8a587052079d2c09701282
davidbarone <davidbarone@live.com>
2020-07-27 17:58:38 +1000

    incorrect url for resource upload.

--- src/components/Resources/ResourcesAdmin.js
+++ src/components/Resources/ResourcesAdmin.js
@@ -7,43 +7,43 @@ class ResourcesAdmin extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
     };
   }
 
   getData = () => {
     fetch(`${process.env.REACT_APP_API_ROOT}/resources`)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         this.setState({ data: data });
       });
   };
 
-  deleteResource = id => {
+  deleteResource = (id) => {
     try {
-      fetch(`http://localhost:5000/resources/${id}`, {
+      fetch(`${process.env.REACT_APP_API_ROOT}/resources/${id}`, {
         method: "DELETE",
         headers: {
-          Authorization: AuthService.getToken()
-        }
-      }).then(res => {
-        this.setState(prevState => ({
-          data: prevState.data.filter(el => el.id !== id)
+          Authorization: AuthService.getToken(),
+        },
+      }).then((res) => {
+        this.setState((prevState) => ({
+          data: prevState.data.filter((el) => el.id !== id),
         }));
       });
     } catch (err) {}
   };
 
-  addResource = formData => {
+  addResource = (formData) => {
     try {
-      fetch("http://localhost:5000/resources/upload", {
+      fetch(`${process.env.REACT_APP_API_ROOT}/resources/upload`, {
         method: "POST",
         body: formData,
         headers: {
-          Authorization: AuthService.getToken()
-        }
-      }).then(res => {
-        res.json().then(json => {
+          Authorization: AuthService.getToken(),
+        },
+      }).then((res) => {
+        res.json().then((json) => {
           this.setState({ data: [...this.state.data, json] });
         });
       });
@@ -57,6 +57,10 @@ class ResourcesAdmin extends React.Component {
   render() {
     return (
       <>
+        <div>
+          All resources on this list can be accessed via:
+          https://api.dbarone.com/resources/name/[filename]
+        </div>
         <ResourcesGrid
           data={this.state.data}
           onDeleteResource={this.deleteResource}
